817204982c53533528233e9d40d923de03fe69ef
Can <canacan2170@gmail.com>
2018-10-08 11:16:18 +0300

    Update maxdifference.py

--- maxdifference.py
+++ maxdifference.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+
+# Given array has some integers. 
+# We are trying to find max difference between an integer in array and situated in array more lower index integers.
+
+
 def maxDifference(a):
 
     max_difference = -1
@@ -8,10 +13,10 @@ def maxDifference(a):
         for i2 in range(i1):
 
             if a[i1] >= a[i2]:
-                compare = a[i1] - a[i2]
+                difference = a[i1] - a[i2]
 
-                if compare > max_difference:
-                    max_difference = compare
+                if difference > max_difference:
+                    max_difference = difference
 
     return max_difference
 
@@ -19,23 +24,4 @@ if __name__ == '__main__':
 
     a = [6, 5, 10, 2, 1, 9, 1]
 
-    print(maxDifference(a))
-
-
-
-
-#def maxDifference(a):
-#    max_value_index = 0
-#    max_value = a[max_value_index]
-#
-#    for index in range(len(a)):
-#        if a[index] > max_value:
-#            max_value = a[index]
-#            max_value_index = index
-#
-#max_difference = -1
-#for index in range(len(a[:max_value_index])):
-#    compare = max_value - a[index]
-#    if compare > max_difference:
-#        max_difference = compare
-#return max_difference
+    print(maxDifference(a)) # => 8 Result is 8 because for this example, max difference between 9 - 1 = 8.
